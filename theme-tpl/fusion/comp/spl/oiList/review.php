664b353fa851e07698edca1e795b15bc3b4899aa
Козленко В <www.dft@mail.ru>
2012-07-15 18:16:04 +0400

    Новые файлы для темы fusion

--- theme-tpl/fusion/comp/spl/oiList/review.php
+++ theme-tpl/fusion/comp/spl/oiList/review.php
@@ -1,24 +1,47 @@
 <?$oiListData=self::get('oiListData')?>
-            		<div class="sixteen columns _portfolio function">
+<div class="sixteen columns _portfolio function">
 
-                    <div id="portfolio" class="portfolio-3">
-                        <ul class="portfolio-list">
-                            <?
-							$previewCount = count($oiListData);
-							for($i=0; $i < $previewCount; $i++){
-								$class = $i==0 ? 'alpha' : 'omega';
-							?>
-                            <li class="one-third column item all webdesign <?=$class?>">
-                                <div class="img">
-									<img class="portfolio-overlay-item" src="<?=$oiListData[$i]['prevImgUrl']?>" width="300" height="200" alt="Смотреть" rel="#"/>
-								</div>
-                                <div class="info">
-                                    <h4><?=$oiListData[$i]['caption']?></h4>
-                                    <p><?=$oiListData[$i]['text']?></p>
-                                </div>
-                            </li>
-							<?}?>
-                        </ul>
-                        <div class="clear"></div>
+    <div id="portfolio" class="portfolio-3">
+        <ul class="portfolio-list">
+            <?
+            $previewCount = count($oiListData);
+            for ($i = 0; $i < $previewCount; $i++) {
+                $class = $i == 0 ? 'alpha' : 'omega';
+                ?>
+                <li class="one-third column item all webdesign <?=$class?>">
+                    <div class="img">
+                        <a rel="prettyPhoto" href="<?=$oiListData[$i]['videoUrl']?>">
+                            <img class="portfolio-overlay-item" src="<?=$oiListData[$i]['prevImgUrl']?>" width="300"
+                                 height="200" alt="Смотреть" rel="#"/>
+                        </a>
                     </div>
-            		</div>
+                    <div class="info">
+                        <h4><?=$oiListData[$i]['caption']?></h4>
+                        <p><?=$oiListData[$i]['text']?></p>
+                    </div>
+                </li>
+                <? }?>
+        </ul>
+        <div class="clear"></div>
+    </div>
+    
+</div>
+<script type="text/javascript">
+	function initReview(){
+		$("#portfolio a[rel='prettyPhoto']").prettyPhoto({
+			theme: 'facebook',
+			show_title: false,
+			default_width: 800,
+			default_height: 600
+		});
+	}
+
+	if ( $.prettyPhoto ){
+		initReview();
+	}else{
+		importResList["css"].push('http://theme.codecampus.ru/fusion/prettyPhoto/prettyPhoto.css');
+		
+		var src = 'http://theme.codecampus.ru/fusion/prettyPhoto/jquery.prettyPhoto.js';
+		importResList["js"].push({src: src, func: initReview});
+	}
+</script>
---
+++ theme-tpl/fusion/site/map/map.php
@@ -0,0 +1,21 @@
+<div class="five columns">
+	<h3>Контакты</h3>
+	<div class="contactBox">
+		<p class="skype">iOrator</p>
+		<p class="phone">+7 905 150 12 02</p>
+		<p class="icq">256 985 563</p>
+		<p class="email">iorator@mail.ru</p>
+		<p class="map">Тридевятое царство</p>
+	</div>
+	
+	<p>
+		<a href="#" class="small_button" style="width: 200px;">Распечатать карту проезда</a>
+	</p>
+	<p>
+		<a href="#" class="small_button" style="width: 200px;">Открыть в картах Yandex</a>
+	</p>
+	
+</div>
+<div class="eleven columns">
+	<img alt="img" src="/res/images/map.png" style="width: 640px" alt="Карта проезда">
+</div>
